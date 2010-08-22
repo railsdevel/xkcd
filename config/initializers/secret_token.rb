47e0d99723cafaff6a443439fa4dbf1dace19bf1
Utkarsh Kukreti <railsdevel@gmail.com>
2010-08-23 01:44:38 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XKCD::Application.config.secret_token = 'ab25b244e48c216fda1d688dd7fa2a1d405d342327c48498b8030ffadcc0eb98358652ce73e19aeedd94e8a27db6dcee13e9a529b8b63fbd5cec0d95c6deec9b'
